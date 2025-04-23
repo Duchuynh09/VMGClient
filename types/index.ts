6ee17a35c0157ed7be70fc463b3239dfc35b1b43
Duc <tanghuynh09@gmail.com>
2025-04-23 16:31:07 +0700

    thêm kiểu dữ liệu cho stage và product

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,32 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export interface Product {
+  Id: string;
+  style: string;
+  customer: string;
+  color: string[];
+  unitPrice: number;
+  startDate: string;
+}
+
+export interface Stage {
+  stageID: string;
+  productId: string;
+  stageName: string;
+  timeWork: number;
+  description: string;
+  unitPriceSecond: number;
+  unitPriceProductivity: number;
+  unitPriceMain: number;
+  department: number;
+  Product: {
+    style: string;
+    customer: string;
+  };
+  Department: {
+    departmentName: string;
+  }
+}
+
